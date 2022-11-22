b2b7765b1da38965ecc43071660f5678eaf46a03
guruvshenoy <guruvshenoy@gmail.com>
2022-11-22 11:16:43 +0530

    test cases for init tests

---
+++ testcases/test_feature1.py
@@ -0,0 +1,15 @@
+def test_case1():
+    print("Test case 1")
+    assert 5+5==10
+
+def test_case2():
+    print("Test case 2")
+    assert 5//2==1,"Failed test case 2"
+
+def test_case3():
+    print("Test case 3")
+    assert 5//2==1,"Failed test case 3"
+
+def test_case4():
+    print("Test case 4")
+    assert 9//5==1
